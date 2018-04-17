fd50d7b1e74f1e3f1f7b73e7189d57c871510a77
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-04-17 18:02:45 -0400

    Minor Changes

--- src/main/train.py
+++ src/main/train.py
@@ -130,4 +130,4 @@ def train(start_epoch=0, epochs=10, resume_frm_chck_pt=True, force_save_model=Fa
     print("#########################################################")
 
 if __name__ == "__main__":
-    train(start_epoch=0, epochs=1, resume_frm_chck_pt=False, force_save_model=False, restrict_seqlen=-1)
+    train(start_epoch=0, epochs=1, resume_frm_chck_pt=False, force_save_model=False, restrict_seqlen=2)
