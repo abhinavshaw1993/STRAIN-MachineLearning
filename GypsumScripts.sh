63fb90978db8a708c3d514b553b572842466d689
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-04-17 16:28:36 -0400

    GypSum Slurm script.

---
+++ GypsumScripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --mem=30000
+#SBATCH --job-name=1-gpu-STRAIN-custom
+#SBATCH --partition=m40-short
+#SBATCH --output=STRAIN-custom-%A.out
+#SBATCH --error=STRAIN-custom-%A.err
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=abhinavshaw@umass.edu
+
+# Log the jobid.
+echo $SLURM_JOBID - `hostname` >> ~/slurm-jobs.txt
+python ~/projects/STRAIN-MachineLearning/src/main/train.py
+
