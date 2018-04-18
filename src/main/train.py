e2c85d04a85295044157c57b1edf0e2e42e5308b
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-04-18 00:08:02 -0400

    1 Epoch Train, on whole Data.

--- src/main/train.py
+++ src/main/train.py
@@ -138,4 +138,4 @@ def train(start_epoch=0, epochs=10, resume_frm_chck_pt=True, force_save_model=Fa
 
 
 if __name__ == "__main__":
-    train(start_epoch=0, epochs=10, resume_frm_chck_pt=False, force_save_model=False, restrict_seqlen=5)
+    train(start_epoch=0, epochs=1, resume_frm_chck_pt=False, force_save_model=False, restrict_seqlen=-1)
