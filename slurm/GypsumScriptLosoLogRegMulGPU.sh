0e38f00be57b5720865131c6a6e3546d297d55f8
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-08-06 18:21:45 -0400

    Changes in Bash Script

--- slurm/GypsumScriptLosoLogRegMulGPU.sh
+++ slurm/GypsumScriptLosoLogRegMulGPU.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --mem=60000
 #SBATCH --job-name=1-gpu-STRAIN-custom
-#SBATCH --partition=m40-short
+#SBATCH --partition=titanx-short
 #SBATCH --output=STRAIN-loso_log_reg_loso_mul_gpu-%A.out
 #SBATCH --error=STRAIN-loso_log_reg_loso_mul_gpu-%A.err
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:6
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=abhinavshaw@umass.edu
 
