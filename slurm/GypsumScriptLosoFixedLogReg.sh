82eb82ba6c20e2d722dc0a478f96f6c712f2787a
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-08-10 17:26:52 -0400

    Changes

--- slurm/GypsumScriptLosoFixedLogReg.sh
+++ slurm/GypsumScriptLosoFixedLogReg.sh
@@ -2,9 +2,9 @@
 #
 #SBATCH --mem=60000
 #SBATCH --job-name=1-gpu-STRAIN-custom
-#SBATCH --partition=m40-long
-#SBATCH --output=STRAIN-loso_log_reg_loso-%A.out
-#SBATCH --error=STRAIN-loso_log_reg_loso-%A.err
+#SBATCH --partition=m40-short
+#SBATCH --output=STRAIN-loso_log_reg_loso_fixed_seq-%A.out
+#SBATCH --error=STRAIN-loso_log_reg_loso_fixed_seq-%A.err
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=abhinavshaw@umass.edu
