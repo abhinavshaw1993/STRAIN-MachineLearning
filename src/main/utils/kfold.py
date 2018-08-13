2108b9ae57b36f38d2bb954f31df6d667f41cd70
Abhinav Shaw <abhinav.shaw1993@gmail.com>
2018-08-13 16:13:04 -0400

    K fold Split for OverFitting.

--- src/main/utils/kfold.py
+++ src/main/utils/kfold.py
@@ -3,6 +3,10 @@
 
 def get_splits(data_list):
 
-    for idx, val_data in enumerate(data_list):
-        yield data_list[:idx] + data_list[idx+1:], val_data
+    if len(data_list) == 1:
+        yield data_list, data_list[0]
+
+    else:
+        for idx, val_data in enumerate(data_list):
+            yield data_list[:idx] + data_list[idx+1:], val_data
 
